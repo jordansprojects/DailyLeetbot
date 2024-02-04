148f316746dd507d96667242e0b068f5b608b086
jordansprojects <jordsvpn@protonmail.com>
2024-02-03 22:13:53 -0800

    added selenium webscraping, removed unnecessary comments

--- dlcp.py
+++ dlcp.py
@@ -4,18 +4,68 @@ import bs4
 from datetime import datetime
 import pytz # timezone conversions
 import sys
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-
+# Scrapes leetcode problemset page for the daily question so the link can be retrieved and posted by the bot
+# This is a Wonderful resource on selenium for web scraping purposes :
+#    https://www.browserstack.com/guide/web-scraping-using-selenium-python
 def get_daily_challenge():
+    WAIT_TIME = 5
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--no-sandbox");
+    chrome_options.add_argument("--disable-dev-shm-usage");
+    chrome_options.add_argument("--disable-renderer-backgrounding");
+    chrome_options.add_argument("--disable-background-timer-throttling");
+    chrome_options.add_argument("--disable-backgrounding-occluded-windows");
+    chrome_options.add_argument("--disable-client-side-phishing-detection");
+    chrome_options.add_argument("--disable-crash-reporter");
+    chrome_options.add_argument("--disable-oopr-debug-crash-dump");
+    chrome_options.add_argument("--no-crash-upload");
+    chrome_options.add_argument("--disable-gpu");
+    chrome_options.add_argument("--disable-extensions");
+    chrome_options.add_argument("--disable-low-res-tiling");
+    chrome_options.add_argument("--log-level=3");
+    chrome_options.add_argument("--silent");
+    driver=webdriver.Chrome(options=chrome_options)
     logger = open ('logs/dlcp.log', 'w')
     # our link will contain this data
-    query_str =  '?envType=daily-question&envId=' + datetime.now(pytz.utc).strftime('%Y-%m-%d') # leetcode uses UTC
+    query_str =  'envType=daily-question&envId=' + datetime.now(pytz.utc).strftime('%Y-%m-%d') # leetcode uses UTC
     logger.write(sys.argv[0] + " Using query string " + query_str + '\n')
-    url_to_scrape = 'https://leetcode.com/problemset/all/' 
+    url_to_scrape = 'https://leetcode.com/problemset/' 
     response = requests.get(url_to_scrape)
-        
+    
     # url we will append to once we find the daily question element
     base_url = 'https://leetcode.com'
+    wait = WebDriverWait(driver, WAIT_TIME)
+    driver.get(url_to_scrape)
+    get_url = driver.current_url
+    wait.until(EC.url_to_be(url_to_scrape))
+    if(get_url != url_to_scrape):
+        raise Exception(sys.argv[0] + "URL does not match.\nExpected : " + url_to_scrape + "\nActual: " + get_url )
+
+    page_source = driver.page_source
+    driver.quit()
+    soup = bs4.BeautifulSoup(page_source, features="html.parser")
+    for a in soup.body.find_all('a', href=True):
+        #logger.write("url contained " + match['href'] + '\n')
+        if(query_str in a['href']):
+            url = base_url + a['href']
+            logger.write("match found : " + a['href'])
+            return url
+
+
+
+   
+    
     logger.close()
 
 def get_advent_of_code():
--- leetbot.py
+++ leetbot.py
@@ -5,29 +5,29 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 import dlcp as dlcp
 import pytz # for timezone conversions
-
+import traceback
 EST = pytz.timezone("US/Eastern")
 UTC = pytz.utc
 
 TARGET_CHANNEL_NAME =  "programming"
 XMAS_EMOJIS1 = " :christmas_tree: :santa: :snowman2: "
 XMAS_EMOJIS2 = " :snowman2: :santa: :christmas_tree: "
-WELCOME = "Hello. I am Daily LeetCode Problem bot. I will post the Leetcode question everytime the new questions drops at 4:00PM PST (12:00AM UTC)"
+WELCOME = "Hello. I will post the Leetcode question everytime the new questions drops at 4:00PM PST (12:00AM UTC)"
 
 class DailyLeetClient(commands.Bot):
+    
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.logger= open ('logs/leetbot.log', 'w')
 
     async def on_ready(self):
         print(f'leetbot.py: Logged on as {self.user}!')
-        #init scheduler
         scheduler = AsyncIOScheduler()
-        # add scheduler jobs
-        # aws uses UTC, so the parameters are based on UTC military time until I add support for timezone conversion
-        scheduler.add_job(self.post_daily_question,CronTrigger(hour="0",minute ="0", second="0")) # 4pm PST = 12am UTC when LeetCode problem of day changes
+        scheduler.add_job(self.post_daily_question,CronTrigger(hour="16",minute ="0", second="0")) # 4pm PST = 12am UTC when LeetCode problem of day changes
+        #debug
+        #scheduler.add_job(self.post_daily_question, CronTrigger(second="0"))
         scheduler.add_job(self.post_advent_of_code, CronTrigger(hour="5", minute="0", second="0",month="12" ))  # 9pm PST = 12am EST = 5AM UTC  when advent of code is posted
-        # start scheduler
         scheduler.start()
 
         for server in self.guilds:
@@ -39,8 +39,8 @@ class DailyLeetClient(commands.Bot):
          for server in self.guilds:
             for channel in server.channels:
                 if isinstance(channel, discord.TextChannel) and channel.name == TARGET_CHANNEL_NAME:
-                    #await self.target_channel.send("Problem of the day " + dlcp.get_daily_challenge() )
-                    await channel.send("Hello.")
+                    await channel.send("Problem of the day " + dlcp.get_daily_challenge() )
+                    #await channel.send("Hello.")
         
     async def post_advent_of_code(self): 
         for server in self.guilds:
@@ -48,6 +48,11 @@ class DailyLeetClient(commands.Bot):
                 if isinstance(channel, discord.TextChannel) and channel.name == TARGET_CHANNEL_NAME:
                     await channel.send(XMAS_EMOJIS1 +  "Ho ho ho! Advent of code challenge of the day : " + dlcp.get_advent_of_code()
                                            + XMAS_EMOJIS2)
+                    
+    async def on_error(self, event_method, *args, **kwargs):
+        self.logger.write(traceback.format_exc())
+
+
 
 
     
